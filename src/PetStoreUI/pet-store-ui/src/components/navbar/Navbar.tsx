f0d09a01d959705d5a6460584fb96c63e7ba7789
Ameya Shinde <ameyashinde123@outlook.com>
2023-08-21 02:39:54 +0530

    Made navbar responsive.

--- src/PetStoreUI/pet-store-ui/src/components/navbar/Navbar.tsx
+++ src/PetStoreUI/pet-store-ui/src/components/navbar/Navbar.tsx
@@ -1,19 +1,31 @@
 import './navbar.scss'
-import { Menu } from "@mui/icons-material"
+import { Close, Menu } from "@mui/icons-material"
+import { useState } from 'react'
 import { Link } from "react-router-dom"
 
 const Navbar: React.FC = () => {
+    const [open, setOpen] = useState<boolean>(false);
+
+    const toggleNavBar = () => {
+        if (window.innerWidth < 500) {
+            setOpen(!open);
+        }
+    };
+    
+    const menuStyle = open ? "menu open" : "menu";
+
     return (
         <div className="navbar">
             <div className="brand">Pet Store</div>
             <div className="hamburger">
-                <Menu />
+                <Menu onClick={toggleNavBar} />
             </div>
-            <div className="menu">
+            <div className={menuStyle}>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/products">Products</Link></li>
-                    <li><Link to="/products/add">Add Product</Link></li>
+                    <Close className='close' onClick={toggleNavBar} />
+                    <li onClick={toggleNavBar}><Link to="/">Home</Link></li>
+                    <li onClick={toggleNavBar}><Link to="/products">Products</Link></li>
+                    <li onClick={toggleNavBar}><Link to="/products/add">Add Product</Link></li>
                 </ul>
             </div>
         </div>
